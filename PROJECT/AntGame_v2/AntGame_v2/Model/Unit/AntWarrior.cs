7b10ef6460eadf9610506a6333cfaa67c2bd049e
Alex <sanchopa@mail.ru>
2015-12-10 13:56:25 +0600

    Добавил классы юнитов

---
+++ PROJECT/AntGame_v2/AntGame_v2/Model/Unit/AntWarrior.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntGame_v2.Model.Unit
+{
+    class AntWarrior : Unit
+    {
+    }
+}
