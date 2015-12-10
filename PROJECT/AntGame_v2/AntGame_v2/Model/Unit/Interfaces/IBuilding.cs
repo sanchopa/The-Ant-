578ae4221bbccee16c80131b0519a633958bc806
Alex <sanchopa@mail.ru>
2015-12-10 12:21:59 +0600

    Добавил интерфейсы юнитов

---
+++ PROJECT/AntGame_v2/AntGame_v2/Model/Unit/Interfaces/IBuilding.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntGame_v2.Model
+{
+    public interface IBuilding
+    {
+        void Build();
+    }
+}
