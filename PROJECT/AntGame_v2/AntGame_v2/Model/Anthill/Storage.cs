2e0617bd6c6269c727fc2f5d8558df7d0e433ff8
Snurnicyn Konstantin <ks_on_v@mail.ru>
2015-12-17 10:41:14 +0600

    Update Storage.cs

--- PROJECT/AntGame_v2/AntGame_v2/Model/Anthill/Storage.cs
+++ PROJECT/AntGame_v2/AntGame_v2/Model/Anthill/Storage.cs
@@ -8,27 +8,27 @@ namespace AntGame_v2.Model.Unit
 {
     class Storage : Building
     {
-        Dictionary<string, int> resources { get; set; }//При создании экземпляра класса Муравейник, необходимо в его констркторе заполнить словарь resources четко прописанными видами ресурсов
+        Dictionary<string, int> _resources { get; set; }//При создании экземпляра класса Муравейник, необходимо в его констркторе заполнить словарь resources четко прописанными видами ресурсов
         
         public int GetResource(string res)
         {
-           return this.resources[res].Value;
+           return this._resources[res].Value;
         }
         
         public void UpResource(string res, int count)
         {
-            if(this.resources[res] + count > buildingLevel * 100)//От уровня зависит вместимость хранилища
-              this.resources[res] = buildingLevel * 100;
+            if(this._resources[res] + count > buildingLevel * 100)//От уровня зависит вместимость хранилища
+              this._resources[res] = buildingLevel * 100;
             else
-              this.resources[res] += count;
+              this._resources[res] += count;
         }
         
         public void DownResource(string res, int count)
         {
-            if(this.resources[res] - count < 0)
-              this.resources[res] = 0;
+            if(this._resources[res] - count < 0)
+              this._resources[res] = 0;
             else 
-              this.resources[res] -= count;
+              this._resources[res] -= count;
         }
     }
 }
